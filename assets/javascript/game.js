be508623c473303a1e0b6ec4ca9b44945a725faf
timhayes00 <timhayes00@gmail.com>
2018-03-05 22:10:08 -0500

    saving updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+// STOPWATCH ACTIVITY (SOLUTION)
+// =============================
+
+// This code will run as soon as the page loads
+window.onload = function() {
+    // $("#lap").on("click", stopwatch.recordLap);
+    // $("#stop").on("click", stopwatch.stop);
+    // $("#reset").on("click", stopwatch.reset);
+    // $("#start").on("click", stopwatch.start);
+  };
+  
+  var questionsObject = {
+    "q1": "What is the air speed velocity of a fully laden European swallow?",
+    "q2": "How many humans are there in the Disney movie 'Bambi'?",
+    "q3": "When he won his first Masters in 1997, at the time Tiger Woods was the youngest golfer to do so.  How old was he?",
+    "q4": "What year was Prohibition prohibited in the USA?",
+    "a1": ["11 meters per second", "8 meters per second", "14 meters per second"],
+    "a2": ["Zero", "One", "Two", "Three"],
+    "a3": ["19", "20", "21", "22"],
+    "a4": ["1930", "1931", "1932", "1933"],
+    "a1correct": "11 meters per second",
+    "a2correct": "Zero",
+    "a3correct": "21",
+    "a4correct": "1933"
+  }
+
+
+
+
